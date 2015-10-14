018db082a81ee8608dc37ea7d6f04cdd3f45d52e
gaojun610 <gaojun610gj@gmail.com>
2015-10-14 15:52:07 +0800

    保存

---
+++ string/asort.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jun90610@gmail.com
+ * Date: 2015/10/14
+ * Time: 14:57
+ */
+
+$a = 123;
+$b = strrev($a);
+echo $b . "\n";
+
+$c = 659;
+$e = decbin($c);
+$f = strrev($e);
+echo bindec($f);

9ee254e2055e213b3d6b8b525892f97062007205
1148743058@qq.com <1148743058@qq.com>
2016-01-17 14:47:35 +0800

    保存

--- string/null.php
+++ string/null.php
@@ -3,11 +3,16 @@ header('content-type:text/html;charset=utf-8;');
 echo null == false ? '等于' : '不等于';
 echo "\n";
 if(null == false){
-	echo '等于';
+	echo 'null == false等于';
 }else{
-	echo '不等于';
+	echo 'nul == false不等于';
 }	
-
+echo '<br>';
+if(null === false){
+	echo 'null === false 等于';
+}else{
+	echo 'null === false 不等于';
+}
 echo "\n";
 
 
